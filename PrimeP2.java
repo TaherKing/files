4250fad88350225346c446af204030f70c68cf72
Taher King <taherking@gmail.com>
2023-01-23 12:02:01 +0530

    modified one file

--- PrimeP2.java
+++ PrimeP2.java
@@ -4,7 +4,7 @@ class PrimeP2
 	public static void main(String[]args)
 	{
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter the number");
+		System.out.println("Enter the number here");
 		int num=sc.nextInt();
 		
 		for(int i=1;i<=num;i++)
